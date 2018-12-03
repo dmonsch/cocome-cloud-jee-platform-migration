015e1fe9049e8915d4e0d32a5577c36a635a3cdb
David Monschein <ugdzy@student.kit.edu>
2018-12-03 23:08:28 +0100

    ---- DEB ---

--- cocome-maven-project/cloud-logic-service/cloud-store-logic/store-logic-ejb/src/main/java/org/cocome/tradingsystem/inventory/application/store/StoreServer.java
+++ cocome-maven-project/cloud-logic-service/cloud-store-logic/store-logic-ejb/src/main/java/org/cocome/tradingsystem/inventory/application/store/StoreServer.java
@@ -393,6 +393,8 @@ public class StoreServer implements Serializable, IStoreInventoryManagerLocal, I
 	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 	private void __bookSale(long storeID, final SaleTO saleTO) 
 			throws ProductOutOfStockException, NotInDatabaseException, UpdateException {
+		debugPrint(String.valueOf(ThreadMonitoringController.getInstance()));
+		
 		for (final ProductWithStockItemTO pwsto : saleTO.getProductTOs()) {
 			final IStockItem si = __storeQuery.queryStockItemById(pwsto
 					.getStockItemTO().getId());
