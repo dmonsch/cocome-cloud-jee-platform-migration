1c3d3ff68d73deb15987f781dbf89be616e7a4e6
David Monschein <david.monschein@novatec-gmbh.de>
2019-08-01 14:16:09 +0200

    Fine granular update again.

--- cocome-maven-project/cloud-logic-service/cloud-logic-core-impl/src/main/java/org/cocome/tradingsystem/inventory/application/store/monitoring/ThreadMonitoringController.java
+++ cocome-maven-project/cloud-logic-service/cloud-logic-core-impl/src/main/java/org/cocome/tradingsystem/inventory/application/store/monitoring/ThreadMonitoringController.java
@@ -31,7 +31,7 @@ import tools.vitruv.applications.pcmjava.modelrefinement.parameters.monitoring.r
  *
  */
 public class ThreadMonitoringController {
-	private static boolean FINE_GRANULAR = false;
+	private static boolean FINE_GRANULAR = true;
 
 	private static final int INITIAL_SERVICE_DEPTH_COUNT = 10;
 
